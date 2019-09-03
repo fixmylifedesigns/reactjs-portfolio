85706589889eac5c9f032f36c515faffc84ce791
fixmylifedesigns <ijd.irving@gmail.com>
2019-09-03 03:26:01 -0400

    resumeshorten

--- src/components/routes/mobileResponsiveResume.js
+++ src/components/routes/mobileResponsiveResume.js
@@ -1,4 +1,5 @@
 import React from "react";
+import resumeData from "./resumeInfo.json";
 
 export default function MobileResponsiveResume() {
   return (
@@ -11,134 +12,44 @@ export default function MobileResponsiveResume() {
               <h3 className="header-resume role">FULL STACK DEVELOPER</h3>
             </div>
             <div className="contact-section">
-              <a
-                href="mailto:contact@duranirving.com"
-                className="resume-link resumePTag mobileLink"
-              >
-                <i
-                  className="fa fa-envelope link-icons"
-                />{" "}
-                contact@duranirving.com
-              </a>
-              <a
-                href="https://www.duranirving.com"
-                className="resume-link resumePTag mobileLink"
-              >
-                {" "}
-                <i
-                className="fa fa-globe link-icons"
-                />{" "}
-                duranirving.com{" "}
-              </a>
-              <a href="tel:347-265-5234" className="resume-link resumePTag mobileLink">
-                {" "}
-                <i
-                className="fa fa-phone link-icons"
-                />{" "}
-                (347) 265-5234{" "}
-              </a>
-              <a
-                href="https://www.google.com/maps/place/Brooklyn,+NY/@40.6451594,-74.0850816,11z/data=!3m1!4b1!4m5!3m4!1s0x89c24416947c2109:0x82765c7404007886!8m2!3d40.6781784!4d-73.9441579"
-                className="resume-link resumePTag mobileLink"
-              >
-                {" "}
-                <i
-                className="fa fa-map-marker link-icons"
-                />{" "}
-                Brooklyn, New York{" "}
-              </a>
-              <a
-                href="https://www.twitter.com/fixmylifenyc"
-                className="resume-link resumePTag mobileLink"
-              >
-                {" "}
-                <i
-                className="fa fa-twitter link-icons"
-                />{" "}
-                fixmylifenyc
-              </a>{" "}
-              <a
-                href="https://www.linkedin.com/in/irvingduran"
-                className="resume-link resumePTag mobileLink"
-              >
-                <i
-                className="fa fa-linkedin link-icons"
-                />{" "}
-                irvingduran{" "}
-              </a>{" "}
-              <a
-                href="https://www.github.com/fixmylifedesigns"
-                className="resume-link resumePTag mobileLink"
-              >
-                <i
-                className="fa fa-github link-icons"
-                />{" "}
-                fixmylifedesigns
-              </a>
+              {resumeData.contact.map(info => {
+                return (
+                  <a
+                    href={info.href}
+                    className="resume-link resumePTag mobileLink"
+                  >
+                    <i className={`${info.class} link-icons`} /> {info.link}
+                  </a>
+                );
+              })}
             </div>
           </div>
           <div className="bottom-content">
             <h2 className="section-titles">Skills</h2>
             <div className="all-skills">
               <div className="skills-container">
-                <div className="skills-content">
-                  <p className="skillPTag bold">CODING LANGUAGES</p>
-                  <p className="skillPTag">• HTML5</p>
-                  <p className="skillPTag">• CSS3</p>
-                  <p className="skillPTag">• Javascript ES5/ ES6</p>
-                  <p className="skillPTag">• Python</p>
-                </div>
-                <div className="skills-content">
-                  <p className="skillPTag bold">FRAMEWORKS AND LIBRARIES</p>
-                  <p className="skillPTag">• ReactJS</p>
-                  <p className="skillPTag">• NodeJs</p>
-                  <p className="skillPTag">• Express</p>
-                  <p className="skillPTag">• LESS/SASS</p>
-                  <p className="skillPTag">• Jest</p>
-                  <p className="skillPTag">• React Testing Library</p>
-                  <p className="skillPTag">• Material UI</p>
-                  <p className="skillPTag">• React Google Maps</p>
-                  <p className="skillPTag">• React Google Location</p>
-                </div>
-                <div className="skills-content">
-                  <p className="skillPTag bold">TOOLS</p>
-                  <p className="skillPTag">• Git</p>
-                  <p className="skillPTag">• GitHub</p>
-                  <p className="skillPTag">• Heroku</p>
-                  <p className="skillPTag">• Netlify</p>
-                  <p className="skillPTag">• Render</p>
-                  <p className="skillPTag">• VS Code</p>
-                  <p className="skillPTag">• Atom</p>
-                  <p className="skillPTag">• Windows 10 OS</p>
-                  <p className="skillPTag">• Terminal</p>
-                  <p className="skillPTag">• Linux</p>
-                </div>
+                {resumeData.skills_section_one.map(info => {
+                  return (
+                    <div className="skills-content">
+                      <p className="skillPTag bold">{info.title}</p>
+                      {info.skills.map(skill => {
+                        return <p className="skillPTag">{skill.skill}</p>;
+                      })}
+                    </div>
+                  );
+                })}
               </div>
               <div className="skills-container">
-                <div className="skills-content">
-                  <p className="skillPTag bold">SOFT SKILLS</p>
-                  <p className="skillPTag">• Problem Solving</p>
-                  <p className="skillPTag">• Time Management</p>
-                  <p className="skillPTag">• Leadership</p>
-                  <p className="skillPTag">• Creative Thinking</p>
-                </div>
-                <div className="skills-content">
-                  <p className="skillPTag bold">DATABASES</p>
-                  <p className="skillPTag">• SQLite</p>
-                  <p className="skillPTag">• PostgreSQL</p>
-                </div>
-                <div className="skills-content">
-                  <p className="skillPTag bold">SOFTWARE DEVELOPMENT</p>
-                  <p className="skillPTag">• RESTful APIs</p>
-                  <p className="skillPTag">• Object Oriented Programming</p>
-                  <p className="skillPTag">• AGILE Development</p>
-                </div>
-                <div className="skills-content">
-                  <p className="skillPTag bold">LANGUAGES</p>
-                  <p className="skillPTag">• English★★★★★</p>
-                  <p className="skillPTag">• Spanish★★★★★</p>
-                  <p className="skillPTag">• French ★☆☆☆☆</p>
-                </div>
+                {resumeData.skills_section_two.map(info => {
+                  return (
+                    <div className="skills-content">
+                      <p className="skillPTag bold">{info.title}</p>
+                      {info.skills.map(skill => {
+                        return <p className="skillPTag">{skill.skill}</p>;
+                      })}
+                    </div>
+                  );
+                })}
               </div>
             </div>
           </div>
@@ -148,186 +59,62 @@ export default function MobileResponsiveResume() {
         <div className="right-text-container">
           <div className="projects-container">
             <h2 className="section-titles">Projects</h2>
-            <div className="project">
-              <a
-                href="https://educell-marketing.netlify.com/"
-                className="resume-link"
-              >
-                <p className="projectTitle">Educell</p>
-              </a>
-              <p className="skillPTag">
-                HTML | CSS w/ LESS | Javascript | Adobe Illustrator
-              </p>
-              <p className="skillPTag">Marketing page for a web application</p>
-              <p className="skillPTag">Role: UI Developer</p>
-              <p className="skillPTag">• Solo Project</p>
-              <p className="skillPTag">
-                • Designed user interface including illustrations, fonts and
-                color scheme
-              </p>
-              <p className="skillPTag">
-                • Responsive View for desktop, tablet and mobile
-              </p>
-            </div>
-            <div className="project">
-              <a href="https://www.intelliflight.app" className="resume-link">
-                <p className="projectTitle">Intelliflight</p>{" "}
-              </a>
-              <p className="skillPTag">
-                Node | ReactJS | React Google Maps | Axios | Auth0 | Adobe
-                Illustrator
-              </p>
-              <p className="skillPTag">
-                Web application that allows pilots to create pireps and route
-                plans
-              </p>
-              <p className="skillPTag">Role: Full Stack Developer </p>
-              <p className="skillPTag">• Team Project</p>
-              <p className="skillPTag">
-                • Designed user interface including illustrations
-              </p>
-              <p className="skillPTag">
-                • Responsive View for desktop, tablet and mobile
-              </p>
-              <p className="skillPTag">• Integrated google maps</p>
-              <p className="skillPTag">
-                • Set up backend database with endpoints and documentation
-              </p>
-              <p className="skillPTag" />
-            </div>
-            <div className="project">
-              <a href="https://art-folio.netlify.com" className="resume-link">
-                <p className="projectTitle">Broken U-Lock</p>
-              </a>
-              <p className="skillPTag">
-              Node | ReactJS | React Google Maps | Axios | PostgreSQL| Adobe Illustrator
-              </p>
-              <p className="skillPTag">
-              Web application that allows free users to view post and logged in users to post, comment and receive notifications
-              </p>
-              <p className="skillPTag">Role: Full Stack Developer</p>
-              <p className="skillPTag">• Solo Project</p>
-              <p className="skillPTag">
-                • Designed user interface including illustrations
-              </p>
-              <p className="skillPTag">
-                • Responsive View for desktop, tablet and mobile
-              </p>
-              <p className="skillPTag">• Register and validating login</p>
-              <p className="skillPTag">
-                • Axios calls to back-end api for storing data and user
-                information
-              </p>
-            </div>
+            {resumeData &&
+              resumeData.projects.map(project => {
+                return (
+                  <div className="project">
+                    <a href={project.url} className="resume-link">
+                      <p className="projectTitle">{project.title}</p>
+                    </a>
+                    {project.skills.map(skill => {
+                      return <p className="skillPTag">{skill.line}</p>;
+                    })}
+                  </div>
+                );
+              })}
           </div>
           <div className="employment-container">
             <h2 className="section-titles">Employment</h2>
-            <div className="employer lambdaschool">
-              <div className="employer-content">
-                <div>
-                  <p className="employerTitle">Lambda School</p>
-                  <p className="sub-title">Team Lead</p>
+            {resumeData.employment.map(info => {
+              return (
+                <div className="employer">
+                  <div className="employer-content">
+                    <div>
+                      <p className="employerTitle">{info.employeer}</p>
+                      <p className="sub-title">{info.title}</p>
+                    </div>
+                    <div className="employment-content">
+                      {info.info.map(info => {
+                        return <p className="skillPTag">{info.line}</p>;
+                      })}
+                    </div>
+                  </div>
+                  {info.description.map(info => {
+                    return <p className="skillPTag">{info.line}</p>;
+                  })}
                 </div>
-                <div className="employment-content">
-                  <p className="skillPTag">Remote</p>
-                  <p className="skillPTag">May 2019 to Current</p>
-                </div>
-              </div>
-              <p className="skillPTag">
-                • Manage a small group of students by taking attendance and
-                grading
-              </p>
-              <p className="skillPTag">
-                • Mentor students to achieve good coding practices
-              </p>
-            </div>
-            <div className="employer fixmylife">
-              <div className="employer-content">
-                <div>
-                  <p className="employerTitle">Fix My Life</p>
-                  <p className="sub-title">Founder</p>
-                </div>
-                <div className="employment-content">
-                  <p className="skillPTag">https://www.fmlcycling.com/</p>
-                  <p className="skillPTag">Dec. 2013 to Current</p>
-                </div>
-              </div>
-              <p className="skillPTag">
-                • Build long-term customer relationships and advise customers on
-                purchases
-              </p>
-              <p className="skillPTag">
-                • Routinely answered customer questions regarding merchandise
-                and pricing
-              </p>
-              <p className="skillPTag">
-                • Develop reputation as an efficient service provider with high
-                levels of accuracy
-              </p>
-            </div>
-            <div className="employer Deliv">
-              <div className="employer-content">
-                <div>
-                  <p className="employerTitle">Deliv co</p>
-                  <p className="sub-title">Independent Contractor</p>
-                </div>
-                <div className="employment-content">
-                  <p className="skillPTag">New York, New York</p>
-                  <p className="skillPTag">Apr. 2016 to Jan. 2019</p>
-                </div>
-              </div>
-              <p className="skillPTag">
-                • Make deliveries and pick-ups of items as requested
-              </p>
-              <p className="skillPTag">
-                • Establish excellent working relationships at all delivery and
-                pickup points
-              </p>
-            </div>
+              );
+            })}
           </div>
           <div className="employment-container">
             <h2 className="section-titles">Education</h2>
-            <div className="employer lambdaschool">
-              <div className="employer-content">
-                <div>
-                  <p className="employerTitle">Lambda School</p>
-                </div>
-                <div className="employment-content">
-                  <p className="skillPTag">Feb. 2019 to Sept. 2019</p>
-                </div>
-              </div>
-              <p className="skillPTag achivement">
-                Certificate of Completion Web Development & Computer Science
-              </p>
-            </div>
-            <div className="employer lambdaschool">
-              <div className="employer-content">
-                <div>
-                  <p className="employerTitle">
-                    New York Code + Design Academy
+            {resumeData.education.map(school => {
+              return (
+                <div className="employer lambdaschool">
+                  <div className="employer-content">
+                    <div>
+                      <p className="employerTitle">{school.school}</p>
+                    </div>
+                    <div className="employment-content">
+                      <p className="skillPTag">{school.duration}</p>
+                    </div>
+                  </div>
+                  <p className="skillPTag achivement">
+                    {school.certification}{" "}
                   </p>
                 </div>
-                <div className="employment-content">
-                  <p className="skillPTag">May 2018 to June 2018</p>
-                </div>
-              </div>
-              <p className="skillPTag achivement">
-                Web Development Foundation 2018
-              </p>
-            </div>
-            <div className="employer lambdaschool">
-              <div className="employer-content">
-                <div>
-                  <p className="employerTitle">Lambda School</p>
-                </div>
-                <div className="employment-content">
-                  <p className="skillPTag">Fall 2012 to Fall 2015</p>
-                </div>
-              </div>
-              <p className="skillPTag achivement">
-                Associates 32 credits completed towards Business Administration
-              </p>
-            </div>
+              );
+            })}
           </div>
         </div>
       </div>
