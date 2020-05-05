b7a0127c8a1a6a5bace6b87ca1c1af4d04b1a5a6
fixmylifedesigns <ijd.irving@gmail.com>
2020-05-05 01:06:05 -0400

    footer

--- src/components/navigation/footer.js
+++ src/components/navigation/footer.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import "../../CSS/footer.css"
+import Button from '@material-ui/core/Button';
+
 
 export default class Footer extends Component {
     render() {
         return (
             <div className="footer-container">
                 <h4>Copyright © Design by Irving Jose Duran 2019 </h4>
+                <Button className="loginButton" variant="contained" href="https://www.app.duranirving.com">Login</Button>
             </div>
         )
     }
